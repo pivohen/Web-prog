952384e656daccc94c341134aef9ace95b15df67
tspu_02 <tspu_02@mail.ru>
2024-10-31 15:48:03 +0700

    start learn data types

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
+# числовые:
+# int
+number_1 = 12 
 
+# float
+number_2 = 15.5     
+
+print(
+    number_1,
+    number_2,
+)
+
+
+#строки
+
+string_1 = 'hello'
+string_2 = "hello"
+string_3 = "hello \\ \"world\" \\ "
+string_4 = "\n"
+
+
+print(
+    string_1,
+    string_4,
+    string_2,
+    string_4,
+    string_3,
+    )
