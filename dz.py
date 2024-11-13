082527367f147efb9385fc7114e388bfc998dcbe
pivohen <143980665+pivohen@users.noreply.github.com>
2024-11-13 20:28:49 +0300

    dz

--- dz.py
+++ dz.py
@@ -1,1 +1,37 @@
-Hello dz!
+'''
+a = int (input('Введите а: '))
+b = int (input('Введите b: '))
+
+def add_numbers( a, b ):
+    sumN = a + b
+    return sumN
+
+print('Ответ:', add_numbers( a, b))
+'''
+
+"""
+x = int
+
+def sum_list(lst):
+    lst = 0
+    for x in lst:
+        sumA = sumA + x
+        return sumA
+
+lst = [1, 3, 5, 7, 11, 13]
+
+result = sum_list(lst)
+print ('Ответ: ', result)
+
+"""
+
+'''
+x = ['Hello', 'world']
+#s = ' 2 '
+line = ', '.join(x)
+print(line)
+
+'''
+
+z = "Пример разделения"
+print(z.split())
